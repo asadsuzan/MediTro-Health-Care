b4845b8e77bc9ca4015a1381fac105f51147ecad
suzan <asadsuzan7@gamil.com>
2022-06-26 16:21:28 +0600

    added fotter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./Pages/Home/Home";
 import Services from "./Pages/Services/Services";
 import ContactUs from "./Pages/ContactUs/ContactUs";
 import { Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/service" element={<Services />} />
         <Route path="/contact" element={<ContactUs />} />
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,80 @@
+import React from "react";
+import logo from "../../assets/logo/logo.png";
+import { MyButtonLg } from "../MyButtons/MyButtons";
+import fb from "../../assets/social/fb.png";
+import linkedin from "../../assets/social/linkdin.png";
+import insta from "../../assets/social/insta.png";
+import tw from "../../assets/social/tw.png";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row g-5">
+          <div className="col xl-4 col-lg-4 col-md-4 col-12">
+            <div>
+              <div className="f-title">
+                <img src={logo} alt="meditro" className="f-logo" />
+              </div>
+              <p className="f-des">
+                Lorem ipsum is dolor sit amet, csectetur adipiscing elit, dolore
+                smod tempor incididunt ut labore et.
+              </p>
+            </div>
+          </div>
+          <div className="col xl-4 col-lg-4 col-md-4 col-12 d-flex justify-content-between">
+            <ul className="f-links list-unstyled">
+              <div className="f-title">Quick Links</div>
+              <li> About Us</li>
+              <li> Services</li>
+              <li>Booking</li>
+              <li>Faq's</li>
+              <li>Blogs</li>
+              <li>Out Team</li>
+            </ul>
+
+            <ul className="f-links list-unstyled">
+              <div className="f-title">Our Service</div>
+              <li> Dental Care</li>
+              <li>Cardiac Clinic</li>
+              <li>Massege Therapy</li>
+              <li>Cardiology</li>
+              <li>Precise Diagnosis</li>
+              <li>Abmbulance Services</li>
+            </ul>
+          </div>
+          <div className="col xl-4 col-lg-4 col-md-4 col-12">
+            <div className="f-title">subscribe now</div>
+            <form className="subscribe-form">
+              <input
+                type="email"
+                placeholder="Email Address"
+                className="form-control shadow-lg p-2"
+              />{" "}
+              <br />
+              <MyButtonLg
+                style={{
+                  color: "white",
+                  background: "#1f2278",
+                  width: "100%",
+                  padding: "10px 0",
+                }}
+              >
+                subscribe now
+              </MyButtonLg>
+            </form>
+            <div className="social-links d-flex">
+              <img src={fb} alt="fb" className="img-fluid " />
+              <img src={insta} alt="fb" className="img-fluid " />
+              <img src={linkedin} alt="fb" className="img-fluid " />
+              <img src={tw} alt="fb" className="img-fluid " />
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
