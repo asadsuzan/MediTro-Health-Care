8f3cf9000b501449a3c820ac76ad8517396e9857
suzan <asadsuzan7@gamil.com>
2022-07-08 21:27:20 +0600

    implement filtering appointments based on admin or non-admin

--- src/Components/UserApointments/UserAppointment.js
+++ src/Components/UserApointments/UserAppointment.js
@@ -14,25 +14,11 @@ import { MdDeleteForever } from "react-icons/md";
 const UserAppointment = () => {
   const [appointment, setAppointment] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState("");
   const [user] = useAuthState(auth);
   const navigate = useNavigate();
   const [isAdmin] = UseAdmin(user);
-
-  // load appointments
-  // useEffect(() => {
-  //   fetch(`http://localhost:5000/my_appointment/${user?.email}`, {
-  //     method: "GET",
-  //     headers: {
-  //       authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-  //     },
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setAppointment(data);
-
-  //       setLoading(false);
-  //     });
-  // }, [user]);
+  const [today, setToday] = useState(new Date().toISOString().slice(0, 10));
 
   // load appointments
   const loadAppointments = (user) => {
@@ -52,7 +38,6 @@ const UserAppointment = () => {
   useEffect(() => {
     loadAppointments(user);
   }, [user]);
-
   // change  pending/complete status  for admin
   const changStatus = (e, data) => {
     const stage = e.target.value;
@@ -88,77 +73,113 @@ const UserAppointment = () => {
   return (
     <div className="appointment-body">
       {appointment.length ? (
-        <table>
-          <thead className="shadow-sm">
-            <tr>
-              <th>#</th>
-              <th>service</th>
-              <th>appointment date</th>
-              <th>booking date</th>
-              <th>amount</th>
-              <th>status</th>
-              {isAdmin && <th>user</th>}
-              {isAdmin && <th>actions</th>}
-            </tr>
-          </thead>
-          <tbody>
-            {appointment?.map((data, index) => {
-              return (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td className="text-dark text-capitalize">
-                    {data.serviceName}
-                  </td>
-                  <td>
-                    <span className="time">{data.appointmentTime}</span> <br />
-                    <span className="date">{data.appointmentDate}</span>
-                  </td>
-                  <td>
-                    <span className="time">{data.bookingTime}</span> <br />
-                    <span className="date">{data.bookingDate}</span>
-                  </td>
-                  <td>${data.amount}</td>
-                  <td className="text-success fw-bold">
-                    <p className={data.stage === "pending" && "text-danger"}>
-                      {data.stage}
-                    </p>
-                  </td>
-                  {isAdmin && (
-                    <td className="td-container" title={data.email}>
-                      {data.email.slice(0, 4) + "..."}
-                      <div className="td-item shadow-lg bg-dark text-light">
-                        {data.email}
-                      </div>
-                    </td>
-                  )}
-                  {isAdmin && (
-                    <td className="text-success fw-bold">
-                      <select
-                        title="change status"
-                        onChange={(e) => changStatus(e, data)}
-                        className="select-tab"
-                      >
-                        <option selected disabled hidden>
-                          status
-                        </option>
+        <>
+          <div className="text-light d-flex fixed">
+            {isAdmin && (
+              <input
+                className="rounded rounded-5"
+                type="text"
+                placeholder="search by email"
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            )}
+            <select className="" onChange={(e) => setQuery(e.target.value)}>
+              <option value="">All</option>
+              <option value="pending">pending</option>
+              <option value="complete">complete</option>
+              <option value={today}>Today</option>
+            </select>
+          </div>
+          <table>
+            <thead className="shadow-sm">
+              <tr>
+                <th>#</th>
+                <th>service</th>
+                <th>appointment date</th>
+                <th>booking date</th>
+                <th>amount</th>
+                <th>status</th>
+                {isAdmin && <th>user</th>}
+                {isAdmin && <th>actions</th>}
+              </tr>
+            </thead>
+            <tbody>
+              {appointment
+                .filter(
+                  (a) =>
+                    a.email.toLowerCase().includes(query) ||
+                    a.stage.toLowerCase().includes(query) ||
+                    a.appointmentDate.includes(query)
+                )
+                ?.map((data, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      <td className="text-dark text-capitalize">
+                        {data.serviceName}
+                      </td>
+                      <td>
+                        <span className="time">{data.appointmentTime}</span>{" "}
+                        <br />
+                        <span
+                          className={`date mt-1 d-inline-block ${
+                            data.appointmentDate === today &&
+                            "bg-light text-success"
+                          }`}
+                        >
+                          {data.appointmentDate}
+                        </span>
+                      </td>
+                      <td>
+                        <span className="time">{data.bookingTime}</span> <br />
+                        <span className="date">{data.bookingDate}</span>
+                      </td>
+                      <td>${data.amount}</td>
+                      <td className="text-success fw-bold">
+                        <p
+                          className={data.stage === "pending" && "text-danger"}
+                        >
+                          {data.stage}
+                        </p>
+                      </td>
+                      {isAdmin && (
+                        <td className="td-container" title={data.email}>
+                          {data.email.slice(0, 4) + "..."}
+                          <div className="td-item shadow-lg bg-dark text-light">
+                            {data.email}
+                          </div>
+                        </td>
+                      )}
+                      {isAdmin && (
+                        <td className="text-success fw-bold">
+                          <select
+                            title="change status"
+                            onChange={(e) => changStatus(e, data)}
+                            className="select-tab"
+                          >
+                            <option selected disabled hidden>
+                              status
+                            </option>
 
-                        <option value="complete">completed</option>
-                        <option value="pending">pending</option>
-                      </select>{" "}
-                      <br />
-                      <button
-                        onClick={() => deleteAppointment(data?._id)}
-                        className="remover-btn"
-                      >
-                        <MdDeleteForever />
-                      </button>
-                    </td>
-                  )}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                            <option value="complete">completed</option>
+                            <option value="pending">pending</option>
+                          </select>{" "}
+                          <br />
+                          <button
+                            title="REMOVE"
+                            onClick={() => deleteAppointment(data?._id)}
+                            className="remover-btn"
+                          >
+                            <MdDeleteForever />
+                          </button>
+                        </td>
+                      )}
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </>
       ) : (
         <div className="text-center d-massage">
           <h3>you have't no appointment yet</h3>
